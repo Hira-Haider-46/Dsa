ad3b267e47e19b1ecd93cc0ad757c21a192e509c
Hira Haider Ali <166338659+Hira-Haider-46@users.noreply.github.com>
2024-07-07 17:03:31 +0500

    Add files via upload

---
+++ Hashing/Chaining.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+using namespace std;
+
+// chaining method (open hashing/ closed addressing) 
+
+struct Node
+{
+    int key;
+    Node *next;
+    Node(int key) : key(key), next(nullptr) {}
+};
+
+class HashTable
+{
+private:
+    Node **table;
+    int size;
+    int hashFunction(int key)
+    {
+        return key % size;
+    }
+
+public:
+    HashTable(int size)
+    {
+        this->size = size;
+        table = new Node *[size];
+        for (int i = 0; i < size; i++) table[i] = nullptr;
+    }
+    ~HashTable()
+    {
+        for (int i = 0; i < size; i++)
+        {
+            Node *curr = table[i];
+            while (curr != nullptr)
+            {
+                Node *prev = curr;
+                curr = curr->next;
+                delete prev;
+            }
+        }
+        delete[] table;
+    }
+    void insertItem(int key)
+    {
+        int index = hashFunction(key);
+        Node *newNode = new Node(key);
+        if (table[index] == nullptr) table[index] = newNode;
+        else
+        {
+            Node *temp = table[index];
+            while (temp->next != nullptr) temp = temp->next;
+            temp->next = newNode;
+        }
+    }
+    void deleteItem(int key)
+    {
+        int index = hashFunction(key);
+        Node *curr = table[index];
+        Node *prev = nullptr;
+        while (curr != nullptr && curr->key != key)
+        {
+            prev = curr;
+            curr = curr->next;
+        }
+        if (curr == nullptr) return;
+        else
+        {
+            if (prev == nullptr) table[index] = curr->next;
+            else prev->next = curr->next;
+            delete curr;
+        }
+    }
+    void displayHashTable()
+    {
+        for (int i = 0; i < size; ++i)
+        {
+            cout << "Index " << i << ": ";
+            Node *temp = table[i];
+            while (temp != nullptr)
+            {
+                cout << temp->key << " -> ";
+                temp = temp->next;
+            }
+            cout << "NULL" << endl;
+        }
+    }
+};
+
+int main()
+{
+    int keys[] = {15, 11, 27, 8, 12};
+    int size = sizeof(keys) / sizeof(keys[0]);
+    HashTable hashTable(7);
+    for (int i = 0; i < size; i++) hashTable.insertItem(keys[i]);
+    hashTable.displayHashTable();
+    hashTable.deleteItem(12);
+    cout << "\nAfter deleting key 12\n";
+    hashTable.displayHashTable();
+    return 0;
+}
